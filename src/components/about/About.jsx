ef071241a2f19fa59cef5f49fe99c7c091393b3f
FEnrique <enriquefrauen02@gmail.com>
2022-06-28 15:55:55 +0200

    Second Commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/Me.jpeg';
+import {FaAward} from 'react-icons/fa';
+import {VscFolderLibrary} from 'react-icons/vsc';
+import {BsLaptop} from 'react-icons/bs';
+import {GiSkills} from 'react-icons/gi';
+import {IoIosBusiness} from 'react-icons/io';
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Studying</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>2+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            <ul>
+              <li><BsLaptop/> Hardworking College Student seeking employment.</li>
+              <li><GiSkills/> Bringing forth a motivated attitude and a variety of powerful skills.</li>
+              <li><IoIosBusiness/> Committed to utilizing my skills to further the mission of a company.</li>
+            </ul>
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+      
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './portfolio.css';
+import NC from '../../assets/Netflix-clone.png';
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={NC} alt="Netflix-Clone-img" />
+          </div>
+
+          <h3>Netflix Clone</h3>
+            <a href="https://github.com/FEnrique/netflix-clone" target='_blank' className='btn'>Github</a>
+            <a href="https://netflix-clone-vscode.web.app/" target='_blank' className='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
